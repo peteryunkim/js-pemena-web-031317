a9bdbdc6c055cd42e2e962b13493176df4374dd0
github username <github email address>
2017-05-02 20:06:56 -0400

    fixed associations?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_many :rides, foreign_key: "driver_id"
 
-  has_many :users, foreign_key: "passenger_id", through: :rides
   has_many :trips, foreign_key: "passenger_id"
-  has_many :rides, foreign_key: "passenger_id"
+  has_many :users, foreign_key: "passenger_id", through: :trips
 
 
 
